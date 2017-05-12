021a8d891466037aaa9e95140ca958cc5892046a
dacube101 <rafael10123564@outlook.com>
2017-05-12 17:14:50 -0400

    finished at least i think

--- pageTheScroll/ViewController.swift
+++ pageTheScroll/ViewController.swift
@@ -17,19 +17,30 @@ class ViewController: UIViewController {
     
     var images = [UIImageView]()
 
+    var contentWidth: CGFloat = 0.0
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        for x in 0...2 {
+        
+                for x in 0...2 {
             let image = UIImage(named: "icon\(x).png")
             let imageView = UIImageView(image: image)
             images.append(imageView)
+                    
             var newx: CGFloat = 0.0
+                    
             newx = view.frame.midX + view.frame.width * CGFloat(x)
             
+            scrollView.addSubview(imageView)
+            
+        contentWidth += newx
+            
+            imageView.frame = CGRect(x: newx - 75, y: (view.frame.height / 2) - 75, width: 150, height: 150)
         }
-        print("count: \(images.count)")
+
+        scrollView.contentSize = CGSize(width: contentWidth, height: view.frame.size.height)
+        
     }
 
 
